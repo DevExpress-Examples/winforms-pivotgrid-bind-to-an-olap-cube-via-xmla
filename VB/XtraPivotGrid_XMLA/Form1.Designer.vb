5decf807c82eb1e08477a6c76bcd17788b46cc29
DevExpressExampleBot <fakeEmail>
2022-06-30 11:12:18 +0400

    Source auto update [skip ci]

--- VB/XtraPivotGrid_XMLA/Form1.Designer.vb
+++ VB/XtraPivotGrid_XMLA/Form1.Designer.vb
@@ -1,113 +1,113 @@
-Imports Microsoft.VisualBasic
-Imports System
 Namespace XtraPivotGrid_XMLA
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class Form1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-            Dim PivotGridGroup1 As DevExpress.XtraPivotGrid.PivotGridGroup = New DevExpress.XtraPivotGrid.PivotGridGroup()
-            Dim DataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
-            Dim DataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
+
+            MyBase.Dispose(disposing)
+        End Sub
+
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim pivotGridGroup1 As DevExpress.XtraPivotGrid.PivotGridGroup = New DevExpress.XtraPivotGrid.PivotGridGroup()
+            Dim dataSourceColumnBinding1 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding2 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
+            Dim dataSourceColumnBinding3 As DevExpress.XtraPivotGrid.DataSourceColumnBinding = New DevExpress.XtraPivotGrid.DataSourceColumnBinding()
             Me.pivotGridControl1 = New DevExpress.XtraPivotGrid.PivotGridControl()
             Me.field = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.field1 = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.fieldYear = New DevExpress.XtraPivotGrid.PivotGridField()
             Me.repositoryItemProgressBar1 = New DevExpress.XtraEditors.Repository.RepositoryItemProgressBar()
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.repositoryItemProgressBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.repositoryItemProgressBar1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
-            '
-            'pivotGridControl1
-            '
-            Me.pivotGridControl1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
-            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+            ' 
+            ' pivotGridControl1
+            ' 
+            Me.pivotGridControl1.Anchor = CType(((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) Or System.Windows.Forms.AnchorStyles.Left) Or System.Windows.Forms.AnchorStyles.Right)), System.Windows.Forms.AnchorStyles)
             Me.pivotGridControl1.Fields.AddRange(New DevExpress.XtraPivotGrid.PivotGridField() {Me.field, Me.field1, Me.fieldYear})
-            PivotGridGroup1.Caption = "Year - Month"
-            Me.pivotGridControl1.Groups.AddRange(New DevExpress.XtraPivotGrid.PivotGridGroup() {PivotGridGroup1})
+            pivotGridGroup1.Caption = "Year - Month"
+            Me.pivotGridControl1.Groups.AddRange(New DevExpress.XtraPivotGrid.PivotGridGroup() {pivotGridGroup1})
             Me.pivotGridControl1.Location = New System.Drawing.Point(12, 12)
             Me.pivotGridControl1.Name = "pivotGridControl1"
             Me.pivotGridControl1.RepositoryItems.AddRange(New DevExpress.XtraEditors.Repository.RepositoryItem() {Me.repositoryItemProgressBar1})
             Me.pivotGridControl1.Size = New System.Drawing.Size(651, 281)
             Me.pivotGridControl1.TabIndex = 0
-            '
-            'field
-            '
+            ' 
+            ' field
+            ' 
             Me.field.Area = DevExpress.XtraPivotGrid.PivotArea.DataArea
             Me.field.AreaIndex = 0
             Me.field.Caption = "Sales"
-            DataSourceColumnBinding1.ColumnName = "[Measures].[Internet Sales Amount]"
-            Me.field.DataBinding = DataSourceColumnBinding1
+            dataSourceColumnBinding1.ColumnName = "[Measures].[Internet Sales Amount]"
+            Me.field.DataBinding = dataSourceColumnBinding1
             Me.field.Name = "field"
-            '
-            'field1
-            '
+            ' 
+            ' field1
+            ' 
             Me.field1.Area = DevExpress.XtraPivotGrid.PivotArea.RowArea
             Me.field1.AreaIndex = 0
             Me.field1.Caption = "Country"
-            DataSourceColumnBinding2.ColumnName = "[Customer].[Country].[Country]"
-            Me.field1.DataBinding = DataSourceColumnBinding2
+            dataSourceColumnBinding2.ColumnName = "[Customer].[Country].[Country]"
+            Me.field1.DataBinding = dataSourceColumnBinding2
             Me.field1.Name = "field1"
-            '
-            'fieldYear
-            '
+            ' 
+            ' fieldYear
+            ' 
             Me.fieldYear.Area = DevExpress.XtraPivotGrid.PivotArea.ColumnArea
             Me.fieldYear.AreaIndex = 0
             Me.fieldYear.Caption = "Year"
-            DataSourceColumnBinding3.ColumnName = "[Date].[Fiscal].[Fiscal Year]"
-            Me.fieldYear.DataBinding = DataSourceColumnBinding3
+            dataSourceColumnBinding3.ColumnName = "[Date].[Fiscal].[Fiscal Year]"
+            Me.fieldYear.DataBinding = dataSourceColumnBinding3
             Me.fieldYear.Name = "fieldYear"
-            '
-            'repositoryItemProgressBar1
-            '
+            ' 
+            ' repositoryItemProgressBar1
+            ' 
             Me.repositoryItemProgressBar1.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder
             Me.repositoryItemProgressBar1.DisplayFormat.FormatString = "{0}%"
             Me.repositoryItemProgressBar1.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Custom
             Me.repositoryItemProgressBar1.Name = "repositoryItemProgressBar1"
             Me.repositoryItemProgressBar1.ShowTitle = True
-            '
-            'Form1
-            '
-            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
             Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
             Me.ClientSize = New System.Drawing.Size(675, 305)
             Me.Controls.Add(Me.pivotGridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.pivotGridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.repositoryItemProgressBar1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.pivotGridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.repositoryItemProgressBar1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
 #End Region
-
         Private pivotGridControl1 As DevExpress.XtraPivotGrid.PivotGridControl
-		Private repositoryItemProgressBar1 As DevExpress.XtraEditors.Repository.RepositoryItemProgressBar
-		Private field As DevExpress.XtraPivotGrid.PivotGridField
-		Private field1 As DevExpress.XtraPivotGrid.PivotGridField
-		Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
-	End Class
-End Namespace
 
+        Private repositoryItemProgressBar1 As DevExpress.XtraEditors.Repository.RepositoryItemProgressBar
+
+        Private field As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private field1 As DevExpress.XtraPivotGrid.PivotGridField
+
+        Private fieldYear As DevExpress.XtraPivotGrid.PivotGridField
+    End Class
+End Namespace
